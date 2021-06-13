33eba2ec49fcf5cb6a76cedf7fc11533fde1ecfb
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-06-13 16:58:27 +0900

    Initial commit

---
+++ lib/cellular_sample.ex
@@ -0,0 +1,18 @@
+defmodule CellularSample do
+  @moduledoc """
+  Documentation for CellularSample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CellularSample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cellular_sample_test.exs
@@ -0,0 +1,8 @@
+defmodule CellularSampleTest do
+  use ExUnit.Case
+  doctest CellularSample
+
+  test "greets the world" do
+    assert CellularSample.hello() == :world
+  end
+end
